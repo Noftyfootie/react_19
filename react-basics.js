345394b75d45582b77fe7da22f8f13ad49ef8be6
Noftyfootie <owolabinofisat7@gmail.com>
2026-07-21 20:20:52 +0100

    UPDATE

--- react-basics.js
+++ react-basics.js
@@ -1,2 +1,7 @@
 console.log("Hello React");
 console.log("Props");
+
+const containerElem = chatMessagesRef.current;
+if (containerElem) {
+  containerElem.scrollTop = containerElem.scrollHeight;
+}
