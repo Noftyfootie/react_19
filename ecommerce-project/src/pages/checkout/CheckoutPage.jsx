6223d7e374cb45d9a21d1375670112146ea029b0
Noftyfootie <owolabinofisat7@gmail.com>
2026-08-01 22:31:06 +0100

    Data

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -9,6 +9,7 @@ export function CheckoutPage() {
 
       <CheckoutHeader />
       <div className="checkout-page">
+        {/* whatis this */}
         <div className="page-title">Review your order</div>
 
         <div className="checkout-grid">
