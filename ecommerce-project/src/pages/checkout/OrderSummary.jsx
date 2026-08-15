48217b087858374b6c96634e5ace73e87c1d0b34
Noftyfootie <owolabinofisat7@gmail.com>
2026-08-15 14:11:28 +0100

    Fix quantity error

--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -3,7 +3,7 @@ import { DeliveryOptions } from "./DeliveryOptions";
 import { DeliveryDate } from "./DeliveryDate";
 
 export function OrderSummary({ cart, deliveryOptions, loadCart }) {
-  console.log();
+  console.log("");
   return (
     <div className="order-summary">
       {deliveryOptions.length > 0 &&
