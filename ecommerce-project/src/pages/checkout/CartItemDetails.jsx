42fd7b87f65daede0be310a84728c5498080dbc0
Noftyfootie <owolabinofisat7@gmail.com>
2026-08-17 23:25:09 +0100

    Make keyboard event  to the quantity input

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -28,6 +28,17 @@ export function CartItemDetails({ cartItem, loadCart }) {
     setQuantity(event.target.value);
   };
 
+  const handleQuantityKeyDown = (event) => {
+    const keyPressed = event.key;
+
+    if (keyPressed === "Enter") {
+      updateQuantity();
+    } else if (keyPressed === "Escape") {
+      setQuantity(cartItem.quantity);
+      setIsUpdatingQuantity(false);
+    }
+  };
+
   return (
     <>
       <img className="product-image" src={cartItem.product.image} />
@@ -46,6 +57,7 @@ export function CartItemDetails({ cartItem, loadCart }) {
                 className="inputUpdate"
                 value={quantity}
                 onChange={updateQuantityInput}
+                onKeyDown={handleQuantityKeyDown}
               />
             ) : (
               <span className="quantity-label">{cartItem.quantity}</span>
